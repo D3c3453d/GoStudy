208248443dc6358c1c6e3224eea1504907dc5d71
D3c3453d <sergey.et02@gmail.com>
2022-06-22 17:44:57 +0300

    added: functions for commands

--- main.go
+++ main.go
@@ -2,57 +2,69 @@ package main
 
 import "fmt"
 
+func help(dict map[string]map[string]string) {
+	fmt.Println("/add to add new account")
+	fmt.Println("/all to see all accounts")
+	fmt.Println("/desc to see description of the account")
+	fmt.Println("/phone to see phone of the account")
+}
+
+func add(dict map[string]map[string]string) {
+	var userName string
+	var userPhone string
+	var userDesc string
+
+	fmt.Print("Enter your username: ")
+	fmt.Scanln(&userName)
+	fmt.Print("Enter your phone number: ")
+	fmt.Scanln(&userPhone)
+	fmt.Print("Enter your description: ")
+	fmt.Scanln(&userDesc)
+	dict[userName] = map[string]string{}
+	dict[userName]["Phone"] = userPhone
+	dict[userName]["Description"] = userDesc
+}
+
+func all(dict map[string]map[string]string) {
+	for userName := range dict {
+		fmt.Println(userName)
+	}
+}
+
+func phone(dict map[string]map[string]string) {
+	var userName string
+	fmt.Print("Enter username: ")
+	fmt.Scanln(&userName)
+	fmt.Printf("%s's phone number: %s\n", userName, dict[userName]["Phone"])
+}
+
+func desc(dict map[string]map[string]string) {
+	var userName string
+	fmt.Print("Enter username: ")
+	fmt.Scanln(&userName)
+	fmt.Printf("%s's description: %s\n", userName, dict[userName]["Description"])
+}
+
 func main() {
-	fmt.Println("/help for help")
 	dict := map[string]map[string]string{}
 	var input string
 	for {
 		fmt.Scanln(&input)
-		if input == "/help" {
-			fmt.Println("/add to add new account")
-			fmt.Println("/all to see all accounts")
-			fmt.Println("/desc to see description of the account")
-			fmt.Println("/phone to see phone of the account")
-		}
-		if input == "/add" {
-			var userName string
-			var userPhone string
-			var userDesc string
-
-			fmt.Print("Enter your username: ")
-			fmt.Scanln(&userName)
-			fmt.Print("Enter your phone number: ")
-			fmt.Scanln(&userPhone)
-			fmt.Print("Enter your description: ")
-			fmt.Scanln(&userDesc)
-			dict[userName] = map[string]string{}
-			dict[userName]["Phone"] = userPhone
-			dict[userName]["Description"] = userDesc
-		}
-		if input == "/all" {
-			for userName := range dict {
-				fmt.Println(userName)
-			}
-		}
-		if input == "/all" {
-			for userName := range dict {
-				fmt.Println(userName)
-			}
-		}
-		if input == "/phone" {
-			var userName string
-			fmt.Print("Enter username: ")
-			fmt.Scanln(&userName)
-			fmt.Printf("%s's phone number:\n%s\n", userName, dict[userName]["Phone"])
-		}
-		if input == "/desc" {
-			var userName string
-			fmt.Print("Enter username: ")
-			fmt.Scanln(&userName)
-			fmt.Printf("%s's description:\n%s\n", userName, dict[userName]["Description"])
-		}
-		if input == "/exit" {
+		switch input {
+		case "/help":
+			help(dict)
+		case "/add":
+			add(dict)
+		case "/all":
+			all(dict)
+		case "/phone":
+			phone(dict)
+		case "/desc":
+			desc(dict)
+		case "/exit":
 			break
+		default:
+			fmt.Println("/help for help")
 		}
 	}
 }
