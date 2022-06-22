3f73391ad64e1b770171a638ae03736fa02836e2
D3c3453d <sergey.et02@gmail.com>
2022-06-22 16:05:24 +0300

    start of the dictionary: added /help and /add

--- main.go
+++ main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("/help for help")
+	dict := map[string]map[string]string{}
+	var input string
+	for {
+		fmt.Scanln(&input)
+		if input == "/help" {
+			fmt.Println("/add to add new account")
+			fmt.Println("/all to see all accounts")
+			fmt.Println("/desc to see description of the account")
+			fmt.Println("/phone to see phone of the account")
+		}
+		if input == "/add" {
+			var userName string
+			var userPhone string
+			var userDesc string
+
+			fmt.Print("Enter your username: ")
+			fmt.Scanln(&userName)
+			fmt.Print("Enter your phone number: ")
+			fmt.Scanln(&userPhone)
+			fmt.Print("Enter your description: ")
+			fmt.Scanln(&userDesc)
+			dict[userName]["Phone"] = userPhone
+			dict[userName]["Description"] = userDesc
+		}
+	}
 }
