55556c863e5f7adde9060c800d19a0bc66a1fe87
D3c3453d <sergey.et02@gmail.com>
2022-08-07 23:20:48 +0300

    moved console app init and interaction from main to server file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,63 +2,11 @@ package main
 
 import (
 	"GoStudy/internal/config"
-	"GoStudy/internal/user/handler"
-	"GoStudy/internal/user/repository"
-	"GoStudy/internal/user/service"
-	"GoStudy/pkg/database/postgres"
-	"fmt"
+	"GoStudy/server"
 	_ "github.com/jackc/pgx/v4/stdlib"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	var command config.Commands
-	var dbConf config.DBConfig
-	command.LoadConfig("./commands.env")
-	dbConf.LoadConfig("./db.env")
-
-	db, err := postgres.NewPostgresDB(postgres.Config{
-		Host:     "db",
-		Port:     "5432",
-		Username: dbConf.Username,
-		Password: dbConf.Password,
-		DBName:   dbConf.DBName,
-		SSLMode:  "disable",
-	})
-	if err != nil {
-		logrus.Fatal("Cant create ", err)
-	}
-
-	repos := repository.NewRepository(db)
-	services := service.NewService(repos)
-	handlers := handler.NewHandler(services)
-
-	//interaction
-	var input string
-	for {
-		_, err := fmt.Scan(&input)
-		if err != nil {
-			logrus.Warnln(err)
-		}
-		switch input {
-		case command.Help:
-			handlers.Help(&command)
-		case command.Add:
-			handlers.Add()
-		case command.All:
-			handlers.All()
-		case command.Phone:
-			handlers.Phone()
-		case command.Desc:
-			handlers.Desc()
-		case command.Find:
-			handlers.Find()
-		case command.Show:
-			handlers.Show()
-		case command.Exit:
-			return
-		default:
-			fmt.Printf("%s for help\n", command.Help)
-		}
-	}
+	cfg := config.NewConfig()
+	server.ConsoleApp(cfg)
 }
