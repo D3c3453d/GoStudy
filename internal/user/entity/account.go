ca91d792100726e425d07002cca52e273e70e1bd
D3c3453d <sergey.et02@gmail.com>
2022-08-10 08:40:12 +0300

    added jason tag

--- internal/user/entity/account.go
+++ internal/user/entity/account.go
@@ -1,8 +1,8 @@
 package entity
 
 type Account struct {
-	UserId    string `db:"id"`
-	UserName  string `db:"name"`
-	UserPhone string `db:"phone"`
-	UserDesc  string `db:"description"`
+	UserId    string `db:"id" json:"-"`
+	UserName  string `db:"name" json:"name"`
+	UserPhone string `db:"phone" json:"phone"`
+	UserDesc  string `db:"description" json:"description"`
 }
