ac5415346b12819e34be568f82f56fe633d40a5d
D3c3453d <sergey.et02@gmail.com>
2022-08-10 08:42:35 +0300

    server start

--- internal/server/app.go
+++ internal/server/app.go
@@ -6,11 +6,44 @@ import (
 	"GoStudy/internal/user/repository"
 	"GoStudy/internal/user/service"
 	"GoStudy/pkg/database/postgres"
-	"fmt"
+	"context"
 	"github.com/jmoiron/sqlx"
 	"github.com/sirupsen/logrus"
+	"net/http"
+	"time"
 )
 
+type Server struct {
+	httpServer *http.Server
+}
+
+func NewApp(config *config.Config) {
+	db := initPostgresDB(&config.DBConfig)
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+	srv := new(Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		logrus.Fatal("Server run error ", err)
+	}
+}
+
+func (s *Server) Run(port string, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:         ":" + port,
+		Handler:      handler,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	return s.httpServer.ListenAndServe()
+
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
+
+/*
 func ConsoleApp(config *config.Config) {
 	db := initPostgresDB(&config.DBConfig)
 	repos := repository.NewRepository(db)
@@ -46,7 +79,7 @@ func ConsoleApp(config *config.Config) {
 		}
 	}
 }
-
+*/
 func initPostgresDB(config *config.DBConfig) *sqlx.DB {
 	db, err := postgres.NewPostgresDB(postgres.Config{
 		Host:     "db",
