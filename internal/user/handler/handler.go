b4434dbe33d571a8ca46a2a024f9d1fb106fa068
D3c3453d <sergey.et02@gmail.com>
2022-08-10 08:43:19 +0300

    init routes for handler

--- internal/user/handler/handler.go
+++ internal/user/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"GoStudy/internal/user/service"
+	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
@@ -10,4 +11,16 @@ type Handler struct {
 
 func NewHandler(services *service.Service) *Handler {
 	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+	accounts := router.Group("/accounts")
+	{
+		accounts.POST("/", h.add)
+		accounts.GET("/", h.all)
+		accounts.GET("/phone/:phone", h.find)
+		accounts.GET("/name/:name", h.show)
+	}
+	return router
 }
