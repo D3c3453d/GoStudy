895629a9ac2ac83ea37c45505de2fd966b61badf
D3c3453d <sergey.et02@gmail.com>
2022-08-10 08:41:29 +0300

    new functions for working with http requests

--- internal/user/handler/accounts.go
+++ internal/user/handler/accounts.go
@@ -3,129 +3,67 @@ package handler
 import (
 	"GoStudy/internal/config"
 	"GoStudy/internal/user/entity"
-	"fmt"
-	"github.com/sirupsen/logrus"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func (h *Handler) Help(c *config.Commands) {
-	h.services.Help(c)
-}
-func (h *Handler) Add() {
-	var account entity.Account
-
-	fmt.Print("Enter your username:\n")
-	_, err := fmt.Scan(&account.UserName)
-	if err != nil {
-		logrus.Warnln(err)
-		return
-	}
-	fmt.Print("Enter your phone number:\n")
-	_, err = fmt.Scan(&account.UserPhone)
-	if err != nil {
-		logrus.Warnln(err)
-		return
-	}
-	fmt.Print("Enter your description:\n")
-	_, err = fmt.Scan(&account.UserDesc)
-	if err != nil {
-		logrus.Warnln(err)
-		return
-	}
-	err = h.services.Create(account)
-	if err != nil {
-		logrus.Warnln(err)
-		return
-	}
+func (h *Handler) help(c *gin.Context, cfg *config.Commands) {
+	h.services.Help(cfg)
 }
 
-func (h *Handler) All() {
-	list, err := h.services.GetAll()
-	if err != nil {
-		logrus.Warnln(err)
+func (h *Handler) add(c *gin.Context) {
+	var input entity.Account
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
-	for i, account := range list {
-		fmt.Printf("%d:\n  Name: %s\n  Phone: %s\n  Description: %s\n",
-			i+1, account.UserName, account.UserPhone, account.UserDesc)
+	err := h.services.Create(input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
 	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "added",
+	})
 }
 
-func (h *Handler) Phone() {
-	var userName string
-	fmt.Print("Enter username:\n")
-	_, err := fmt.Scan(&userName)
-	if err != nil {
-		logrus.Warnln(err)
-		return
-	}
-	list, err := h.services.GetByName(userName)
-	if err != nil {
-		logrus.Warnln(err)
-		return
-	}
-	for i, account := range list {
-		fmt.Printf("%d:\n  Name: %s\n  Phone: %s\n",
-			i+1, account.UserName, account.UserPhone)
-	}
+type allResponse struct {
+	Data []entity.Account `json:"accounts"`
 }
 
-func (h *Handler) Desc() {
-	var userName string
-	fmt.Print("Enter username:\n")
-	_, err := fmt.Scan(&userName)
+func (h *Handler) all(c *gin.Context) {
+	list, err := h.services.AccountsServiceI.GetAll()
 	if err != nil {
-		logrus.Warnln(err)
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
 		return
 	}
-	list, err := h.services.GetByName(userName)
-	if err != nil {
-		logrus.Warnln(err)
-		return
-	}
-	if err != nil {
-		logrus.Warnln(err)
-		return
-	}
-	for i, account := range list {
-		fmt.Printf("%d:\n  Name: %s\n  Description: %s\n",
-			i+1, account.UserName, account.UserDesc)
-	}
+
+	c.JSON(http.StatusOK, allResponse{
+		Data: list,
+	})
 }
 
-func (h *Handler) Show() {
-	var userName string
-	fmt.Print("Enter username:\n")
-	_, err := fmt.Scan(&userName)
-	if err != nil {
-		logrus.Warnln(err)
-		return
-	}
-	list, err := h.services.GetByName(userName)
-	if err != nil {
-		logrus.Warnln(err)
-		return
-	}
-	for i, account := range list {
-		fmt.Printf("%d:\n  Name: %s\n  Phone: %s\n  Description: %s\n",
-			i+1, account.UserName, account.UserPhone, account.UserDesc)
-	}
+func (h *Handler) phone(c *gin.Context) {
+
 }
 
-func (h *Handler) Find() {
-	var userPhone string
-	fmt.Print("Enter phone number:\n")
-	_, err := fmt.Scan(&userPhone)
+func (h *Handler) desc(c *gin.Context) {
+
+}
+
+func (h *Handler) show(c *gin.Context) {
+
+}
+
+func (h *Handler) find(c *gin.Context) {
+	phone := c.Param("phone")
+
+	list, err := h.services.AccountsServiceI.GetByPhone(phone)
 	if err != nil {
-		logrus.Warnln(err)
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
 		return
 	}
-	list, err := h.services.GetByPhone(userPhone)
-	if err == nil {
-		for _, account := range list {
-			fmt.Printf("  Name: %s\n  Phone: %s\n  Description: %s\n",
-				account.UserName, account.UserPhone, account.UserDesc)
-		}
-	} else {
-		fmt.Println("Not found")
-	}
+
+	c.JSON(http.StatusOK, list)
+
 }
