e1adc7a9ea16d2faa55aaaa4b98897d20c41f841
D3c3453d <sergey.et02@gmail.com>
2022-08-11 09:26:49 +0300

    removed Help() and Create() from accounts service

--- internal/user/service/accounts.go
+++ internal/user/service/accounts.go
@@ -1,10 +1,8 @@
 package service
 
 import (
-	"GoStudy/internal/config"
 	"GoStudy/internal/user/entity"
 	"GoStudy/internal/user/repository"
-	"fmt"
 )
 
 type AccountsService struct {
@@ -13,21 +11,6 @@ type AccountsService struct {
 
 func NewAccountsService(repo repository.AccountsReposI) *AccountsService {
 	return &AccountsService{repo: repo}
-}
-
-func (s *AccountsService) Help(c *config.Commands) {
-	fmt.Printf("%s to add new account\n", c.Add)
-	fmt.Printf("%s to see all accounts\n", c.All)
-	fmt.Printf("%s to see description of the account\n", c.Desc)
-	fmt.Printf("%s to see phone number of the account\n", c.Phone)
-	fmt.Printf("%s to find account by phone number\n", c.Find)
-	fmt.Printf("%s to show all information about account\n", c.Show)
-	fmt.Printf("%s to exit\n", c.Exit)
-
-}
-
-func (s *AccountsService) Create(account entity.Account) error {
-	return s.repo.Create(account)
 }
 
 func (s *AccountsService) GetAll() ([]entity.Account, error) {
